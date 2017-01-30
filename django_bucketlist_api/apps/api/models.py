812f0807c7370416869160cfb7d8e198244dda2d
Saman M <maftoun95@gmail.com>
2017-01-30 14:03:24 -0800

    commenting begins

--- django_bucketlist_api/apps/api/models.py
+++ django_bucketlist_api/apps/api/models.py
@@ -9,8 +9,11 @@ class Task(models.Model):
     objective = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # Return the objective in string form. for py2
     def __str__(self):
         return self.objective
 
+    # Return the objective in string form. for py3
     def __unicode__(self):
         return self.objective
+        
